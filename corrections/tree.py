ab8c21cbac740245059a0aed467380696bf53d8c
Adrien Lescourt <adrien.lescourt@gmail.com>
2017-11-16 10:11:28 +0100

    Tree correction with walk

--- corrections/tree.py
+++ corrections/tree.py
@@ -14,3 +14,9 @@ def tree(path, level=0, max=10):
             print("| " * level + "-", d.name)
 
 
+def tree2(path):
+    for root, _, files in os.walk(path):
+        depth = root.count(os.sep)
+        print(depth * '| ' + '+ ' + os.path.basename(root))
+        for f in files:
+            print((depth + 1) * '| ' + '- ' + f)
